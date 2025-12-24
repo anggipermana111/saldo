e11fc3599475dbf2e9826eb260d8be57bba41d7c
Anggi Permana <anggipermana490@gmail.com>
2025-12-24 14:25:00 +0700

    Jika tanggal kosong ambil tanggal setelahnya

--- src/main/java/com/dss/saldo/services/PortfolioService.java
+++ src/main/java/com/dss/saldo/services/PortfolioService.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -25,9 +26,12 @@ public class PortfolioService {
 
     @Transactional(readOnly = true)
     public PortfolioSummaryResponse calculatePortfolio() {
+        // Ambil semua transaksi
         List<InvestmentTransaction> transactions = transactionRepo.findAll();
+        // Ambil semua kolom harga unit terakhir
         UnitPrice latestPriceObj = priceRepo.findTopByOrderByNavDateDesc();
 
+        // Handle null, return zero
         if (transactions.isEmpty() || latestPriceObj == null) {
             return PortfolioSummaryResponse.builder()
                     .currentBalance(BigDecimal.ZERO)
@@ -37,34 +41,55 @@ public class PortfolioService {
                     .build();
         }
 
+        // Ambil kolom price saja
         BigDecimal latestPrice = latestPriceObj.getPrice();
 
+        // Hitung jumlah unit
         BigDecimal totalUnits = transactions.stream()
                 .map(InvestmentTransaction::getUnits)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+        // Saldo sekarang
         BigDecimal currentBalance = totalUnits.multiply(latestPrice);
 
+        // Total investasi, deklarasi 0
         BigDecimal totalInvested = BigDecimal.ZERO;
 
+        // Hitung total transaksi dari list transaksi
         for (InvestmentTransaction trx : transactions) {
+            // Mengambil tanggal transaksi + 1 hari
             LocalDate hPlusOneDate = trx.getTransactionDate().plusDays(1);
 
-            UnitPrice historicalPrice = priceRepo.findByNavDate(hPlusOneDate)
-                    .orElseThrow(() -> new RuntimeException("NAV Price not found for date: " + hPlusOneDate));
+            // Mengambil harga unit di tanggal tertentu, throw error jika kosong, jika tanggal kosong ambil tanggal setelahnya
 
+            Optional<UnitPrice> historicalPriceOpt = priceRepo.findByNavDate(hPlusOneDate);
+            UnitPrice historicalPrice = new UnitPrice();
+            while(historicalPriceOpt.isEmpty()) {
+                hPlusOneDate = hPlusOneDate.plusDays(1);
+                historicalPriceOpt = priceRepo.findByNavDate(hPlusOneDate);
+            }
+            historicalPrice = historicalPriceOpt.get();
+
+            // jumlah unit transaksi dikali harga sesuai tanggal
             BigDecimal cost = trx.getUnits().multiply(historicalPrice.getPrice());
+
+            // ditambahkan ke total investasi
             totalInvested = totalInvested.add(cost);
         }
 
+        // method subtract untuk pengurangan
         BigDecimal totalReturn = currentBalance.subtract(totalInvested);
 
+        // Deklarasi persentase keuntungan
         BigDecimal performancePct = BigDecimal.ZERO;
+        // Jika total investasi lebih dari 0
         if (totalInvested.compareTo(BigDecimal.ZERO) > 0) {
+            // Hitung persentase keuntungan
             performancePct = totalReturn.divide(totalInvested, 4, RoundingMode.HALF_UP)
                     .multiply(new BigDecimal("100"));
         }
 
+        // Build return
         return PortfolioSummaryResponse.builder()
                 .currentBalance(currentBalance)
                 .totalInvested(totalInvested)
